cfa20aa60757c74c41e04d3c8b8acf794a3b802e
Fabrice Theoleyre <fabrice.theoleyre@cnrs.fr>
2023-01-05 13:57:28 +0100

    tmp file

--- rfc2pdf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in *.xml
-do
-    filename=`echo $file | cut -d "." -f 1`
-
-    xml2rfc $file
-    cat `echo $filename`.txt  | enscript -qB -o - | ps2pdf - "`echo $filename`.pdf"
-done
